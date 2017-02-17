0082775482be9395f864dc85e67a4cb80842360b
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2017-02-16 19:56:03 -0500

    dox

--- docs/conf.py
+++ docs/conf.py
@@ -129,10 +129,10 @@ html_theme = 'alabaster'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+#html_title = 'poolvr.py'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'poolvr.py'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -155,7 +155,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to poolvr.py's documentation!
-=====================================
+poolvr.py
+=========
 
 Contents:
 
@@ -19,4 +19,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
