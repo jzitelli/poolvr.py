0dc1cbed8a4fc50eb896f62b5204b6ee17754060
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2017-02-18 12:49:19 -0500

    asdf

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,8 +1,11 @@
 import unittest
+import logging
 
 
 from tests.physics_tests import *
 
 
 if __name__ == "__main__":
+    FORMAT = '  POOLVR.PY 0.0.1  | %(asctime)s | %(name)s --- %(levelname)s *** %(message)s'
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
     unittest.main()
