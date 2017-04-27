2dc77ebd49da63cc6230823ee962e1fa4981c0c7
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2017-04-26 23:54:43 -0400

    sound is bad

--- poolvr/ode_physics.py
+++ poolvr/ode_physics.py
@@ -296,8 +296,8 @@ class ODEPoolPhysics(object):
                 c.setMu(0.06)
                 pos, normal, depth, g1, g2 = c.getContactGeomParams()
                 v_n = np.array(normal).dot(np.array(body1.getLinearVel()) - np.array(body2.getLinearVel()))
-                # if abs(v_n) > 0.07:
-                #     #play_ball_ball_collision_sound(vol=(v_n**2 / 5))
-                #     play_ball_ball_collision_sound(vol=max(0.07, min(0.85, v_n**2 / 7)))
+                if abs(v_n) > 0.02:
+                    #play_ball_ball_collision_sound(vol=(v_n**2 / 5))
+                    play_ball_ball_collision_sound(vol=max(0.15, min(0.03, v_n**2 / 7)))
             j = ode.ContactJoint(world, contactgroup, c)
             j.attach(body1, body2)
