e2ce0825a491368a519f66a1c9f08ffba6506327
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2017-06-29 20:50:37 -0400

    imposter normals

--- utils/texutils.py
+++ utils/texutils.py
@@ -1,31 +1,31 @@
 import numpy as np
 import PIL.Image as Image
 
-width, height = 512, 512
+width, height = 128, 128
 multisample = 4
 _width, _height = multisample * width, multisample * height
-img = np.zeros((_width, _height, 3), dtype=np.uint8)
-# Draw something (http://stackoverflow.com/a/10032271/562769)
+
+
+img = np.zeros((_width, _height, 4), dtype=np.uint8)
 x = np.linspace(-1, 1, _width)
 y = np.linspace(-1, 1, _height)
 xv, yv = np.meshgrid(x, y)
-#xx, yy = np.mgrid[:_width, :_height]
-#circle = (xx - _width//2 + 0.5)**2 + (yy - height//2 + 0.5)**2
-# Set the RGB values
-img[xv**2 + yv**2 < 0.975**2*np.ones(xv.shape),:3] = (255, 255, 255)
-# for y in range(img.shape[0]):
-#     for x in range(img.shape[1]):
-#         #r, g, b = circle[y][x], circle[y][x], circle[y][x]
-#         #if circle[y][x] < (_width//2-multisample)**2:
-#         if xy[y,x]**2) < 0.98**2:
-#             r, g, b = 255, 255, 255
-#         else:
-#             r, g, b = 0, 0, 0
-#         img[y][x][0] = r
-#         img[y][x][1] = g
-#         img[y][x][2] = b
-####################################
-image = Image.new('RGB', (_width, _height))
-image.putdata([(int(r), int(g), int(b)) for (r,g,b) in img.reshape(-1,3)])
+img[xv**2 + yv**2 < 0.975**2*np.ones(xv.shape),:4] = (255, 255, 255, 255)
+image = Image.new('RGBA', (_width, _height))
+image.putdata([(int(r), int(g), int(b), int(a)) for (r,g,b,a) in img.reshape(-1,4)])
 image = image.resize((width, height), resample=Image.LANCZOS)
-image.save('out.png')
+image.save('mask.png')
+
+
+normals = np.zeros((_width, _height, 3))
+
+normals[...,0] = xv
+normals[...,1] = yv
+normals[...,2] = np.sqrt(0.975**2 - xv**2 - yv**2)
+normals = np.nan_to_num(normals)
+normals[xv**2 + yv**2 > 0.975**2*np.ones(xv.shape),:3] = -1
+
+image = Image.new('RGB', (_width, _height))
+image.putdata([(int((x+1)/2*255), int((y+1)/2*255), int((z+1)/2*255)) for (x,y,z) in normals.reshape(-1,3)])
+image.resize((width, height), resample=Image.LANCZOS)
+image.save('normals.png')
