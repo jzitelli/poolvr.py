7fd3fa130c1c5309e18ce2ba94b4101b9fc5df7c
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2017-07-01 10:23:18 -0400

    asdf

--- utils/texutils.py
+++ utils/texutils.py
@@ -9,7 +9,7 @@ def gen_sphere_billboards(width=256, height=256, multisample=4,
     _width, _height = multisample * width, multisample * height
     x, y = np.linspace(-1, 1, _width), np.linspace(-1, 1, _height)
     xv, yv = np.meshgrid(x, y)
-    mask = xv**2 + yv**2 < 0.975**2*np.ones(xv.shape)
+    mask = xv**2 + yv**2 < 0.975**2 #*np.ones(xv.shape)
 
     if fp_alpha:
         img = np.zeros((_width, _height, 4), dtype=np.uint8)
@@ -23,14 +23,11 @@ def gen_sphere_billboards(width=256, height=256, multisample=4,
 
     if fp_normal:
         normals = np.zeros((_width, _height, 3))
-        normals[...,0] = xv
-        normals[...,1] = yv
-        normals[mask,2] = np.sqrt(0.975**2*np.ones(xv.shape)[mask] - xv[mask]**2 - yv[mask]**2)
+        normals[mask,0] = xv[mask] / 0.975
+        normals[mask,1] = yv[mask] / 0.975
+        normals[mask,2] = np.sqrt(1.0 - (normals[mask,0]**2 + normals[mask,1]**2))
         normals[~mask,0] = xv[~mask] / np.sqrt(xv[~mask]**2 + yv[~mask]**2)
         normals[~mask,1] = yv[~mask] / np.sqrt(xv[~mask]**2 + yv[~mask]**2)
-        normals[~mask,2] = 0
-        # normals[xv**2 + yv**2 > 0.975**2*np.ones(xv.shape),1] = 1
-        # normals[xv**2 + yv**2 > 0.975**2*np.ones(xv.shape),:3] = -1
         image = Image.new('RGB', (_width, _height))
         image.putdata([(int((x+1)/2*255), int((-y+1)/2*255), int((z+1)/2*255)) for (x,y,z) in normals.reshape(-1,3)])
         image = image.resize((width, height), resample=Image.LANCZOS)
