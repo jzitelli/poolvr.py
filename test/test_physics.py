3fcb94b638390a0f93b26ba7938ab54c531efade
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2018-07-08 21:13:59 -0400

    asdf

--- test/test_physics.py
+++ test/test_physics.py
@@ -1,6 +1,5 @@
 import os.path
 import logging
-import traceback
 import numpy as np
 import pytest
 
@@ -11,7 +10,7 @@ _logger = logging.getLogger(__name__)
 from poolvr.cue import PoolCue
 from poolvr.table import PoolTable
 from poolvr.physics import PoolPhysics
-from poolvr.physics.events import CueStrikeEvent, BallSlidingEvent, BallRollingEvent, BallRestEvent
+from poolvr.physics.events import CueStrikeEvent, BallSlidingEvent, BallRollingEvent, BallRestEvent, BallCollisionEvent
 
 
 from .utils import plot_ball_motion, plot_energy
@@ -28,9 +27,8 @@ def pool_table():
 
 @pytest.fixture()
 def pool_physics(request, pool_table):
-    return PoolPhysics(initial_positions=np.array(pool_table.calc_racked_positions(), dtype=np.float64),                       
+    return PoolPhysics(initial_positions=np.array(pool_table.calc_racked_positions(), dtype=np.float64),
                        use_simple_ball_collisions=True)
-
 
 
 @pytest.fixture
@@ -47,7 +45,7 @@ def test_strike_ball(pool_physics):
     cue = PoolCue()
     cue.velocity[2] = -0.6
     events = physics.strike_ball(0.0, 0, r_c, cue.velocity, cue.mass)
-    _logger.debug('strike on %d resulted in %d events:\n%s', 0, len(events),
+    _logger.debug('strike on %d resulted in %d events:\n\n%s\n', 0, len(events),
                   physics.events_str(events))
     assert 4 == len(events)
     assert isinstance(events[0], CueStrikeEvent)
@@ -71,6 +69,13 @@ def test_ball_collision(pool_physics):
                                    omega_0=np.zeros(3, dtype=np.float64))
     events = physics.add_event_sequence(start_event)
     _logger.debug('%d events added:\n\n%s\n', len(events), physics.events_str(events=events))
+    assert 6 == len(events)
+    assert isinstance(events[0], BallSlidingEvent)
+    assert isinstance(events[1], BallRollingEvent)
+    assert isinstance(events[2], BallCollisionEvent)
+    assert isinstance(events[3], BallRestEvent)
+    assert isinstance(events[4], BallRollingEvent)
+    assert isinstance(events[5], BallRestEvent)
     plot_ball_motion(0, physics, title=test_name, coords=(0,2),
                      collision_depth=1,
                      filename=os.path.join(PLOTS_DIR, test_name + '.png'),
