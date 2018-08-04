6ba4c773068094e53a231e4c4eb315604d7c7d80
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2018-08-03 22:08:59 -0400

    restore plots asdf

--- test/test_physics.py
+++ test/test_physics.py
@@ -8,8 +8,9 @@ from poolvr.cue import PoolCue
 from poolvr.physics.events import PhysicsEvent, CueStrikeEvent, BallSlidingEvent, BallRollingEvent, BallRestEvent, BallCollisionEvent
 
 
-@pytest.mark.parametrize("ball_collision_model", ['simple'])
-def test_strike_ball(pool_physics, ball_collision_model):
+@pytest.mark.parametrize("ball_collision_model", ['simple', 'marlow'])
+def test_strike_ball(pool_physics, ball_collision_model,
+                     plot_motion_z_position, plot_motion_timelapse):
     physics = pool_physics
     physics.reset(balls_on_table=[0])
     r_c = physics.ball_positions[0].copy()
@@ -27,7 +28,9 @@ def test_strike_ball(pool_physics, ball_collision_model):
 
 
 @pytest.mark.parametrize("ball_collision_model", ['simple'])
-def test_ball_collision(pool_physics, ball_collision_model, gl_rendering):
+def test_ball_collision(pool_physics, ball_collision_model,
+                        plot_motion_z_position, plot_motion_timelapse,
+                        gl_rendering):
     physics = pool_physics
     ball_positions = physics.ball_positions.copy()
     ball_positions[1] = ball_positions[0]; ball_positions[1,2] -= 8 * physics.ball_radius
@@ -48,7 +51,9 @@ def test_ball_collision(pool_physics, ball_collision_model, gl_rendering):
 
 
 @pytest.mark.parametrize("ball_collision_model", ['simple'])
-def test_angled_ball_collision(pool_physics, ball_collision_model, gl_rendering):
+def test_angled_ball_collision(pool_physics, ball_collision_model,
+                               plot_motion_z_position, plot_motion_timelapse,
+                               gl_rendering):
     physics = pool_physics
     ball_positions = physics.ball_positions.copy()
     ball_positions[1] = ball_positions[0]
@@ -76,6 +81,7 @@ def test_angled_ball_collision(pool_physics, ball_collision_model, gl_rendering)
 
 @pytest.mark.parametrize("ball_collision_model", ['simple'])
 def test_sliding_ball_collision(pool_physics, ball_collision_model,
+                                plot_motion_z_position, plot_motion_timelapse,
                                 gl_rendering):
     physics = pool_physics
     ball_positions = physics.ball_positions.copy()
@@ -97,7 +103,9 @@ def test_sliding_ball_collision(pool_physics, ball_collision_model,
 
 
 @pytest.mark.parametrize("ball_collision_model", ['simple'])
-def test_break(pool_physics, ball_collision_model, gl_rendering):
+def test_break(pool_physics, ball_collision_model,
+               plot_motion_z_position, plot_motion_timelapse,
+               gl_rendering):
     physics = pool_physics
     r_c = physics.ball_positions[0].copy()
     r_c[2] += physics.ball_radius
