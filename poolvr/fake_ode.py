4f44232597c372d375ee89f6fc7df5ba36daf2b5
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2019-01-13 16:36:17 -0500

    fix indentation in fake_ode.py

--- poolvr/fake_ode.py
+++ poolvr/fake_ode.py
@@ -1,23 +1,23 @@
 class Body(object):
-	def __init__(self, world):
-		pass
-	def setMass(self, mass):
-		pass
-	def setPosition(self, *args, **kwargs):
-		pass
-	def setQuaternion(self, *args, **kwargs):
-		pass
-	def setLinearVel(self, *args, **kwargs):
-		pass
-	def setAngularVel(self, *args, **kwargs):
-		pass
+    def __init__(self, world):
+        pass
+    def setMass(self, mass):
+        pass
+    def setPosition(self, *args, **kwargs):
+        pass
+    def setQuaternion(self, *args, **kwargs):
+        pass
+    def setLinearVel(self, *args, **kwargs):
+        pass
+    def setAngularVel(self, *args, **kwargs):
+        pass
 
 
 class Mass(object):
-	def __init__(self):
-		pass
-	def setCylinderTotal(self, *args, **kwargs):
-		pass
+    def __init__(self):
+        pass
+    def setCylinderTotal(self, *args, **kwargs):
+        pass
 
 
 class World(object):
