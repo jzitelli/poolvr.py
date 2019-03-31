a840b71def8ecbd0ea8798ac466b5c224bb85d31
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2019-03-31 14:54:57 -0400

    quartic solver wip

--- poolvr/physics/poly_solvers.py
+++ poolvr/physics/poly_solvers.py
@@ -1,5 +1,6 @@
 import logging
 _logger = logging.getLogger(__name__)
+from math import fsum
 import numpy as np
 
 
@@ -7,87 +8,91 @@ PIx2 = np.pi*2
 CUBE_ROOTS_OF_1_ANGLES = PIx2/3 * np.arange(3)
 CUBE_ROOTS_OF_1 = np.exp(1j*CUBE_ROOTS_OF_1_ANGLES)
 
-_ZERO_TOLERANCE = 1e-9
+_ZERO_TOLERANCE = 1e-15
 _ZERO_TOLERANCE_SQRD = _ZERO_TOLERANCE**2
 _IMAG_TOLERANCE = 1e-7
 _IMAG_TOLERANCE_SQRD = _IMAG_TOLERANCE**2
 
 
-def quartic_solve(p):
+def quartic_solve(p, only_real=False):
     if abs(p[-1]) / max(abs(p[:-1])) < _ZERO_TOLERANCE:
         _logger.debug('using cubic solver...')
         return cubic_solve(p[:-1])
-    e, d, c, b, a = p
-    Delta = 256*a**3*e**3 - 192*a**2*b*d*e**2 - 128*a**2*c**2*e**2 + 144*a**2*c*d**2*e - 27*a**2*d**4 \
-          + 144*a*b**2*c*e**2 - 6*a*b**2*d**2*e - 80*a*b*c**2*d*e + 18*a*b*c*d**3 + 16*a*c**4*e \
-          - 4*a*c**3*d**2 - 27*b**4*e**2 + 18*b**3*c*d*e - 4*b**3*d**3 - 4*b**2*c**3*e + b**2*c**2*d**2
-    P = 8*a*c - 3*b**2
-    D = 64*a**3*e - 16*a**2*c**2 + 16*a*b**2*c - 16*a**2*b*d - 3*b**4
-    # if Delta > 0 and (P > 0 or D > 0):
-    #     _logger.debug('all roots are complex and distinct')
-    #     return np.empty(0)
-    R = (b**3 - 4*a*b*c + 8*a**2*d)
-    Delta_0 = c**2 - 3*b*d + 12*a*e
+    e, d, c, b, a = p / p[-1]
+    p = (8*c - 3*b**2) / 8
+    q = (b**3 - 4*b*c + 8*d) / 8
+    r = (-3*b**4 + 256*e - 64*b*d + 16*b**2*c) / 256
+    c, d, e = p, q, r
+    cc, dd, ee = c*c, d*d, e*e
+    ccc, eee = cc*c, ee*e
+    cccc, dddd = cc*cc, dd*dd
+    Delta = fsum((-27*dddd, -128*cc*ee, 256*eee, 16*cccc*e, 144*c*dd*e, -4*ccc*dd))
+    _logger.debug('Delta = %s', Delta)
+    D = 64*e - 16*cc
+    p = c
+    q = d
+    if only_real and Delta > 0 and (p > 0 or D > 0):
+        _logger.debug('all roots are complex and distinct')
+        return np.empty(0)
+    R = 8*d
+    Delta_0 = cc + 12*e
     if Delta == 0 and D == 0:
-        if P > 0 and R == 0:
+        if p > 0 and r == 0:
             _logger.debug('two complex-conjugate double roots')
             return np.empty(0)
         elif Delta_0 == 0:
             _logger.debug('all roots are equal to -b / 4a')
-            return np.array([-0.25 * b / a])
-    Delta_1 = 2*c**3 - 9*b*c*d + 27*b**2*e + 27*a*d**2 - 72*a*c*e
-    p = P / (8*a**2)
-    q = R / (8*a**3)
-    _logger.debug('P = %s, D = %s, R = %s\nDelta_0 = %s, Delta_1 = %s',
-                  P, D, R, Delta_0, Delta_1)
-    if Delta > 0:
-        QQQ = (0.5*(Delta_1 + np.sqrt(-27.0*Delta + 0j)))
-    else:
-        QQQ = (0.5*(Delta_1 + np.sqrt(-27.0*Delta)))
-    if Delta > _ZERO_TOLERANCE:
+            return np.array([0.25*b/a])
+    Delta_1 = 2*ccc + 27*dd - 72*c*e
+    if Delta > 0 and p < 0 and D < 0:
         _logger.debug('all roots are real and distinct')
-        Q = QQQ**(1.0/3)
-    elif Delta < -_ZERO_TOLERANCE:
-        _logger.debug('two distinct real roots and a complex-conjugate pair of roots')
-        angle = np.angle(QQQ) / 3
-        if abs(angle) < 1e-8:
-            Q_mag = abs(QQQ)**(1.0/3)
-            Q = Q_mag * np.exp(1j*angle) * CUBE_ROOTS_OF_1[1]
+        phi = np.arccos(Delta_1 / (2*np.sqrt(Delta_0**3)))
+        S = 0.5 * (np.sqrt(-2*p/3 + 2*np.sqrt(Delta_0)*np.cos(phi/3)/3))
+        SSx4 = 4*S**2
+    else:
+        if Delta > 0:
+            sqrt = np.sqrt(-27.0*Delta + 0j)
+            QQQ = (0.5*(Delta_1 + sqrt))
         else:
-            Q = QQQ**(1.0/3)
-    else: # abs(Delta) <= _ZERO_TOLERANCE:
-        _logger.debug('P = %s, D = %s, Delta_0 = %s, R = %s',
-                      P, D, Delta_0, R)
-        if P < 0 and D < 0 and Delta_0 != 0:
-            _logger.debug('one real double root and two other real roots')
-            Q = QQQ**(1.0/3)
-        elif D > 0 or (P > 0 and (D != 0 or R != 0)):
-            _logger.debug('one real double root and a complex-conjugate pair of roots')
-            angle = np.angle(QQQ) / 3
-            if abs(angle) < 1e-8:
-                Q_mag = abs(QQQ)**(1.0/3)
-                Q = Q_mag * np.exp(1j*angle) * CUBE_ROOTS_OF_1[1]
-            else:
-                Q = QQQ**(1.0/3)
-        elif Delta_0 == 0 and D != 0:
-            _logger.debug('one real triple root and one other real root')
-            Q = QQQ**(1.0/3)
-        elif D == 0 and P < 0:
-            _logger.debug('two real double roots')
-            Q = QQQ**(1.0/3)
-    _logger.debug('Delta = %s', Delta)
-    SSx4 = -2.0*p/3 + (Q + Delta_0/Q) / (3.0*a)
-    S = 0.5*np.sqrt(SSx4 if SSx4 >= 0 else SSx4 + 0j)
+            QQQ = (0.5*(Delta_1 + np.sqrt(-27.0*Delta)))
+        Q = QQQ**(1.0/3)
+        SSx4 = -2.0*p/3 + (Q*CUBE_ROOTS_OF_1 + Delta_0/(Q*CUBE_ROOTS_OF_1)) / 3.0
+        if Delta > 0:
+            _logger.debug('all roots are complex and distinct')
+            S = 0.5*np.sqrt(SSx4 + 0j)
+            argsort = np.argsort(abs(S))
+            S = S[argsort[-1]]
+            SSx4 = SSx4[argsort[-1]]
+        elif Delta < 0:
+            _logger.debug('two distinct real roots and a complex-conjugate pair of roots')
+            S = 0.5*np.sqrt(SSx4 + 0j)
+            argsort = np.argsort(abs(S))
+            S = S[argsort[-1]]
+            SSx4 = SSx4[argsort[-1]]
+        else:
+            if p < 0 and D < 0 and Delta_0 != 0:
+                _logger.debug('one real double root and two other real roots')
+            elif D > 0 or (p > 0 and (D != 0 or R != 0)):
+                _logger.debug('one real double root and a complex-conjugate pair of roots')
+            elif Delta_0 == 0 and D != 0:
+                _logger.debug('one real triple root and one other real root')
+            elif D == 0 and p < 0:
+                _logger.debug('two real double roots')
     sqrp = -SSx4 - 2*p + q/S
     sqrm = -SSx4 - 2*p - q/S
     sqrtp = np.sqrt(sqrp if sqrp >= 0 else sqrp + 0j)
     sqrtm = np.sqrt(sqrm if sqrm >= 0 else sqrm + 0j)
-    return np.array((
-        -b/(4*a) - S + 0.5*sqrtp,
-        -b/(4*a) - S - 0.5*sqrtp,
-        -b/(4*a) + S + 0.5*sqrtm,
-        -b/(4*a) + S - 0.5*sqrtm,
+    roots = np.array((
+        -0.25*b - S + 0.5*sqrtp,
+        -0.25*b - S - 0.5*sqrtp,
+        -0.25*b + S + 0.5*sqrtm,
+        -0.25*b + S - 0.5*sqrtm,
     ))
+    return roots
+
+
+def printit(zs):
+    return ',  '.join(str(z) for z in zs)
 
 
 def cubic_solve(p):
--- test/test_poly_solvers.py
+++ test/test_poly_solvers.py
@@ -1,6 +1,24 @@
 import logging
 _logger = logging.getLogger(__name__)
+from math import fsum
 import numpy as np
+
+
+def find_conjugate_pairs(zs):
+    mask = np.array(len(zs)*[True])
+    pairs = []
+    for i, z in enumerate(zs):
+        if z.imag == 0:
+            continue
+        j, z_conj = next(((j, z_conj) for j, z_conj in enumerate(zs[i+1:])
+                          if z.imag + z_conj.imag < 1e-10
+                          and z.real - z_conj.real < 1e-10),
+                         (None, None))
+        if z_conj is not None:
+            pairs.append((z, z_conj))
+            mask[i] = mask[i+j+1] = False
+            zs = zs[mask]
+    return np.array(pairs), zs
 
 
 def test_quartic_solve():
@@ -14,10 +32,9 @@ def test_quartic_solve():
     _logger.debug('''solving x^4 + 9 = 0
 which factors into quadratics
   (x^2 + 6^0.5*x + 3): %s
-  (x^2 - 6^0.5*x + 3): %s
-''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
+  (x^2 - 6^0.5*x + 3): %s''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
     xs = quartic_solve(p)
-    _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
+    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
     xs.sort()
     zs.sort()
     assert((abs(xs-zs) < 1e-7).all())
@@ -30,33 +47,60 @@ which factors into quadratics
     _logger.debug('''solving  x^4 + 2x^3 - x^2 - 2x - 3 = 0
 which factors into quadratics
   (x^2 + x + 1): %s
-  (x^2 + x - 3): %s
-''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
+  (x^2 + x - 3): %s''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
     xs = quartic_solve(p)
-    _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
+    xpairs, xs = find_conjugate_pairs(xs)
     xs.sort()
+    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
+    zpairs, zs = find_conjugate_pairs(zs)
     zs.sort()
+    # _logger.debug('xs = %s\nzs = %s\nabs(xs-zs) = %s',
+    #               ',  '.join(str(c) for c in xs),
+    #               ',  '.join(str(c) for c in zs),
+    #               ',  '.join(str(c) for c in abs(xs-zs)))
     assert((abs(xs-zs) < 1e-7).all())
 
-    for i_poly in range(10):
-        a, b, c, d = xs = np.random.rand(4)
-        p = np.array([
-            1,
-            (-a - b - c - d),
-            (a*b + a*c + a*d + b*c + b*d + c*d),
-            (-a*b*c - a*b*d - a*c*d - b*c*d),
-            a*b*c*d
-        ][::-1])
-        _logger.debug('''solving random quartic #%d:
+    a, b, c, d = xs = np.array((0.5761249714334903, 0.7040881051124456, 0.8687574666374065, 0.868724005945493), dtype=np.float64)
+    p = np.array([
+        1.0,
+        fsum((-a, -b, -c, -d)),
+        a*fsum((b, c, d)) + c*fsum((b, d)) + b*d,
+        a*fsum((-b*c, -b*d, -c*d)) - b*c*d,
+        a*b*c*d
+    ][::-1], dtype=np.float64)
+    _logger.debug('''solving
   x^4 + (%s)x^3 + (%s)x^2 + (%s)x + %s  =  0
 which factors into
   (x - %s)
   (x - %s)
   (x - %s)
-  (x - %s)
-''', i_poly+1, p[3], p[2], p[1], p[0], a, b, c, d)
-        zs = quartic_solve(p)
-        xs.sort()
-        zs.sort()
-        _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
-        assert((abs(xs-zs)/abs(xs) < 1e-7).all())
+  (x - %s)''', p[3], p[2], p[1], p[0], a, b, c, d)
+    Delta = np.prod((xs[1:] - a)**2) * (xs[0] - b)**2 * np.prod((xs[2:] - b)**2) * np.prod((xs[:2] - c)**2) * (xs[3] - c)**2 * np.prod((xs[:-1] - d)**2)
+    _logger.debug('Delta = %s', Delta)
+    zs = quartic_solve(p)
+    xs.sort()
+    zs.sort()
+    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
+    assert((abs(xs-zs)/abs(xs) < 1e-7).all())
+
+#     for i_poly in range(100):
+#         a, b, c, d = xs = np.random.rand(4)
+#         p = np.array([
+#             1,
+#             (-a - b - c - d),
+#             (a*b + a*c + a*d + b*c + b*d + c*d),
+#             (-a*b*c - a*b*d - a*c*d - b*c*d),
+#             a*b*c*d
+#         ][::-1])
+#         _logger.debug('''solving random quartic #%d:
+#   x^4 + (%s)x^3 + (%s)x^2 + (%s)x + %s  =  0
+# which factors into
+#   (x - %s)
+#   (x - %s)
+#   (x - %s)
+#   (x - %s)''', i_poly+1, p[3], p[2], p[1], p[0], a, b, c, d)
+#         zs = quartic_solve(p)
+#         xs.sort()
+#         zs.sort()
+#         _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
+#         assert((abs(xs-zs)/abs(xs) < 1e-7).all())
