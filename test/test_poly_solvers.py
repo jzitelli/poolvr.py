c18be621ff6a77b74e02a87565f648f48f133ec1
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2019-03-31 15:30:08 -0400

    asdf

--- test/test_poly_solvers.py
+++ test/test_poly_solvers.py
@@ -34,7 +34,7 @@ which factors into quadratics
   (x^2 + 6^0.5*x + 3): %s
   (x^2 - 6^0.5*x + 3): %s''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
     xs = quartic_solve(p)
-    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
+    _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
     xs.sort()
     zs.sort()
     assert((abs(xs-zs) < 1e-7).all())
@@ -49,15 +49,12 @@ which factors into quadratics
   (x^2 + x + 1): %s
   (x^2 + x - 3): %s''', ',  '.join(str(x) for x in zs[:2]), ',  '.join(str(x) for x in zs[2:]))
     xs = quartic_solve(p)
+    _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
     xpairs, xs = find_conjugate_pairs(xs)
-    xs.sort()
-    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in xs))
     zpairs, zs = find_conjugate_pairs(zs)
+    xs.sort()
     zs.sort()
-    # _logger.debug('xs = %s\nzs = %s\nabs(xs-zs) = %s',
-    #               ',  '.join(str(c) for c in xs),
-    #               ',  '.join(str(c) for c in zs),
-    #               ',  '.join(str(c) for c in abs(xs-zs)))
+    assert((abs(xpairs-zpairs) < 1e-7).all())
     assert((abs(xs-zs) < 1e-7).all())
 
     a, b, c, d = xs = np.array((0.5761249714334903, 0.7040881051124456, 0.8687574666374065, 0.868724005945493), dtype=np.float64)
@@ -80,27 +77,27 @@ which factors into
     zs = quartic_solve(p)
     xs.sort()
     zs.sort()
-    # _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
+    _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
     assert((abs(xs-zs)/abs(xs) < 1e-7).all())
 
-#     for i_poly in range(100):
-#         a, b, c, d = xs = np.random.rand(4)
-#         p = np.array([
-#             1,
-#             (-a - b - c - d),
-#             (a*b + a*c + a*d + b*c + b*d + c*d),
-#             (-a*b*c - a*b*d - a*c*d - b*c*d),
-#             a*b*c*d
-#         ][::-1])
-#         _logger.debug('''solving random quartic #%d:
-#   x^4 + (%s)x^3 + (%s)x^2 + (%s)x + %s  =  0
-# which factors into
-#   (x - %s)
-#   (x - %s)
-#   (x - %s)
-#   (x - %s)''', i_poly+1, p[3], p[2], p[1], p[0], a, b, c, d)
-#         zs = quartic_solve(p)
-#         xs.sort()
-#         zs.sort()
-#         _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
-#         assert((abs(xs-zs)/abs(xs) < 1e-7).all())
+    for i_poly in range(1000):
+        a, b, c, d = xs = np.random.rand(4)
+        p = np.array([
+            1,
+            (-a - b - c - d),
+            (a*b + a*c + a*d + b*c + b*d + c*d),
+            (-a*b*c - a*b*d - a*c*d - b*c*d),
+            a*b*c*d
+        ][::-1])
+        _logger.debug('''solving random quartic #%d:
+  x^4 + (%s)x^3 + (%s)x^2 + (%s)x + %s  =  0
+which factors into
+  (x - %s)
+  (x - %s)
+  (x - %s)
+  (x - %s)''', i_poly+1, p[3], p[2], p[1], p[0], a, b, c, d)
+        zs = quartic_solve(p)
+        xs.sort()
+        zs.sort()
+        _logger.debug('roots:\n%s', '  '.join(str(x) for x in zs))
+        assert((abs(xs-zs)/abs(xs) < 1e-7).all())
