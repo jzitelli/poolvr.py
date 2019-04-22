9e9e731d2ecd00f9782c52c9ec9841cdd39fb928
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2019-04-22 00:46:34 -0400

    test_gl_rendering

---
+++ test/test_gl_rendering.py
@@ -0,0 +1,55 @@
+import os.path
+_here = os.path.dirname(__file__)
+
+
+from poolvr.gl_rendering import Material, Mesh
+from poolvr.gl_techniques import LAMBERT_TECHNIQUE
+
+
+def test_cone_mesh(gl_rendering, meshes):
+    from poolvr.gl_primitives import ConeMesh
+    material = Material(LAMBERT_TECHNIQUE, values={'u_color': [1.0, 1.0, 0.0, 0.0]})
+    mesh = ConeMesh(material, radius=0.15, height=0.3)
+    for prim in mesh.primitives[material]:
+        prim.attributes['a_position'] = prim.attributes['vertices']
+    mesh.world_matrix[3,2] = -3
+    meshes.append(mesh)
+
+
+def test_frag_box(gl_rendering, meshes):
+    import poolvr
+    from poolvr.gl_rendering import FragBox
+    with open(os.path.join(os.path.dirname(poolvr.__file__), 'shaders', 'sphere_projection_fs.glsl')) as f:
+        fs_src = f.read()
+    mesh = FragBox(fs_src)
+    meshes.append(mesh)
+
+
+def test_sphere_mesh(gl_rendering, meshes):
+    from poolvr.gl_primitives import SpherePrimitive
+    material = Material(LAMBERT_TECHNIQUE, values={'u_color': [0.0, 1.0, 1.0, 0.0]})
+    prim = SpherePrimitive(radius=0.1)
+    prim.attributes['a_position'] = prim.attributes['vertices']
+    mesh = Mesh({material: [prim]})
+    mesh.world_matrix[3,2] = -3
+    meshes.append(mesh)
+
+
+def test_cylinder_mesh(gl_rendering, meshes):
+    from poolvr.gl_primitives import CylinderMesh
+    material = Material(LAMBERT_TECHNIQUE, values={'u_color': [1.0, 1.0, 0.0, 0.0]})
+    mesh = CylinderMesh(material=material, radius=0.15, height=0.5)
+    for prim in mesh.primitives[material]:
+        prim.attributes['a_position'] = prim.attributes['vertices']
+    mesh.world_matrix[3,2] = -3
+    meshes.append(mesh)
+
+
+def test_arrow_mesh(gl_rendering, meshes):
+    from poolvr.gl_primitives import ArrowMesh
+    material = Material(LAMBERT_TECHNIQUE, values={'u_color': [1.0, 1.0, 0.0, 0.0]})
+    mesh = ArrowMesh(material=material)
+    for prim in mesh.primitives[material]:
+        prim.attributes['a_position'] = prim.attributes['vertices']
+    mesh.world_matrix[3,2] = -3
+    meshes.append(mesh)
