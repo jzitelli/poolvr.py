bd108935d11951800a021d8477b7cb34443e84cb
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2019-08-03 22:35:04 -0400

    collisions wip

--- poolvr/physics/collisions.py
+++ poolvr/physics/collisions.py
@@ -12,7 +12,7 @@ This module implements the ball-ball collision model described in: ::
 import logging
 _logger = logging.getLogger(__name__)
 import numpy as np
-
+from numpy import dot, sqrt, cross
 
 INCH2METER = 0.0254
 _k = np.array([0, 0, 1], dtype=np.float64)
@@ -29,41 +29,41 @@ def collide_balls(r_c,
                   g,
                   nP):
     r_ij = r_j - r_i
-    r_ij_mag = np.sqrt(np.dot(r_ij, r_ij))
+    r_ij_mag = sqrt(dot(r_ij, r_ij))
     r_ic = r_c - r_i
     r_jc = r_c - r_j
     z_loc = _k
     y_loc = r_ij / r_ij_mag
-    x_loc = np.cross(y_loc, z_loc)
-    u_iR = v_i + R * np.cross(_k, omega_i)
-    u_iR_mag = np.sqrt(np.dot(u_iR, u_iR))
-    u_jR = v_j + R * np.cross(_k, omega_j)
-    u_jR_mag = np.sqrt(np.dot(u_jR, u_jR))
-    u_iR_x = np.dot(u_iR, x_loc)
-    u_iR_y = np.dot(u_iR, y_loc)
-    u_jR_x = np.dot(u_jR, x_loc)
-    u_jR_y = np.dot(u_jR, y_loc)
-    u_iC = v_i - np.cross(r_ic, omega_i)
-    u_jC = v_j - np.cross(r_jc, omega_j)
+    x_loc = cross(y_loc, z_loc)
+    u_iR = v_i + R * cross(_k, omega_i)
+    u_iR_mag = sqrt(dot(u_iR, u_iR))
+    u_jR = v_j + R * cross(_k, omega_j)
+    u_jR_mag = sqrt(dot(u_jR, u_jR))
+    u_iR_x = dot(u_iR, x_loc)
+    u_iR_y = dot(u_iR, y_loc)
+    u_jR_x = dot(u_jR, x_loc)
+    u_jR_y = dot(u_jR, y_loc)
+    u_iC = v_i - cross(r_ic, omega_i)
+    u_jC = v_j - cross(r_jc, omega_j)
     u_ijC = u_jC - u_iC
-    u_ijC_xz = u_ijC - np.dot(u_ijC, y_loc) * y_loc
-    u_ijC_x = np.dot(u_ijC_xz, x_loc)
-    u_ijC_z = np.dot(u_ijC_xz, z_loc)
-    u_ijC_xz_mag = np.sqrt(np.dot(u_ijC_xz, u_ijC_xz))
-    v_ix, v_iy = np.dot(v_i, x_loc), np.dot(v_i, y_loc)
-    v_jx, v_jy = np.dot(v_j, x_loc), np.dot(v_j, y_loc)
-    omega_ix, omega_iy, omega_iz = np.dot(omega_i, x_loc), np.dot(omega_i, y_loc), np.dot(omega_i, z_loc)
-    omega_jx, omega_jy, omega_jz = np.dot(omega_j, x_loc), np.dot(omega_j, y_loc), np.dot(omega_j, z_loc)
+    u_ijC_xz = u_ijC - dot(u_ijC, y_loc) * y_loc
+    u_ijC_x = dot(u_ijC_xz, x_loc)
+    u_ijC_z = dot(u_ijC_xz, z_loc)
+    u_ijC_xz_mag = sqrt(dot(u_ijC_xz, u_ijC_xz))
     v_ij = v_j - v_i
-    deltaP = 0.5 * (1 + e) * M * np.abs(np.dot(v_ij, y_loc)) / nP
-    v_ijy = np.dot(v_ij, y_loc)
+    v_ijy = dot(v_ij, y_loc)
+    v_ix, v_iy = dot(v_i, x_loc), dot(v_i, y_loc)
+    v_jx, v_jy = dot(v_j, x_loc), dot(v_j, y_loc)
+    omega_ix, omega_iy, omega_iz = dot(omega_i, x_loc), dot(omega_i, y_loc), dot(omega_i, z_loc)
+    omega_jx, omega_jy, omega_jz = dot(omega_j, x_loc), dot(omega_j, y_loc), dot(omega_j, z_loc)
+    deltaP = 0.5 * (1 + e) * M * np.abs(dot(v_ij, y_loc)) / nP
     W_f = float('inf')
     W_c = None
     W = 0
     niters = 0
 
     while v_ijy < 0 or W < W_f:
-        if u_ijC_xz_mag < 1e-9:
+        if u_ijC_xz_mag < 1e-8:
             _logger.debug('no slip at ball-ball contact')
             deltaP_1 = deltaP_2 = 0
             deltaP_ix = deltaP_iy = deltaP_jx = deltaP_jy = 0
@@ -71,83 +71,84 @@ def collide_balls(r_c,
             _logger.debug('slip at ball-ball contact: %s', u_ijC_xz_mag)
             deltaP_1 = -mu_b * deltaP * u_ijC_x / u_ijC_xz_mag
             deltaP_2 = -mu_b * deltaP * u_ijC_z / u_ijC_xz_mag
-            if u_iR_mag < 1e-9:
+            if u_iR_mag < 1e-8:
                 _logger.debug('no slip at i-table contact')
                 deltaP_ix = deltaP_iy = 0
             else:
                 _logger.debug('slip at i-table contact: %s', u_iR_mag)
                 deltaP_ix = -mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_iR_x / u_iR_mag)
                 deltaP_iy = -mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_iR_y / u_iR_mag)
-            if u_jR_mag < 1e-9:
+            if u_jR_mag < 1e-8:
                 _logger.debug('no slip at j-table contact')
                 deltaP_jx = deltaP_jy = 0
             else:
                 _logger.debug('slip at j-table contact: %s', u_jR_mag)
-                deltaP_jx = -mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_jR_x / u_jR_mag)
-                deltaP_jy = -mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_jR_y / u_jR_mag)
-
+                deltaP_jx = mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_jR_x / u_jR_mag)
+                deltaP_jy = mu_b * mu_s * deltaP * (u_ijC_z / u_ijC_xz_mag) * (u_jR_y / u_jR_mag)
+        # velocity changes within plane of impact:
         deltaV_ix = (deltaP_1 + deltaP_ix) / M
         deltaV_iy = (-deltaP + deltaP_iy) / M
-        v_ix0, v_iy0 = v_ix, v_iy
-        v_ix = v_ix0 + deltaV_ix
-        v_iy = v_iy0 + deltaV_iy
         deltaV_jx = (-deltaP_1 + deltaP_jx) / M
         deltaV_jy = (deltaP + deltaP_jy) / M
+        deltaV_ijy = deltaV_jy - deltaV_iy
+        # update velocities:
+        v_ix0, v_iy0 = v_ix, v_iy
+        v_ix         = v_ix0 + deltaV_ix
+        v_iy         = v_iy0 + deltaV_iy
         v_jx0, v_jy0 = v_jx, v_jy
-        v_jx = v_jx0 + deltaV_jx
-        v_jy = v_jy0 + deltaV_jy
-        deltaV_ijy = (v_jy - v_iy) - (v_jy0 - v_iy0)
-        v_ijy0 = v_ijy
-        v_ijy = v_ijy0 + deltaV_ijy
-
+        v_jx         = v_jx0 + deltaV_jx
+        v_jy         = v_jy0 + deltaV_jy
+        v_ijy0       = v_ijy
+        v_ijy        = v_ijy0 + deltaV_ijy
+        v_i = v_ix*x_loc + v_iy*y_loc
+        v_j = v_jx*x_loc + v_jy*y_loc
+        # angular velocity changes (due to frictional forces):
         deltaOm_ix = 5/(2*M*R) * (deltaP_2 + deltaP_iy)
         deltaOm_iy = 5/(2*M*R) * (-deltaP_ix)
         deltaOm_iz = 5/(2*M*R) * (-deltaP_1)
-        omega_ix0 = omega_ix
-        omega_ix = omega_ix0 + deltaOm_ix
-        omega_iy0 = omega_iy
-        omega_iy = omega_iy0 + deltaOm_iy
-        omega_iz0 = omega_iz
-        omega_iz = omega_iz0 + deltaOm_iz
-
         deltaOm_jx = 5/(2*M*R) * (-deltaP_2 + deltaP_jy)
         deltaOm_jy = 5/(2*M*R) * (-deltaP_jx)
         deltaOm_jz = 5/(2*M*R) * (deltaP_1)
+        # update angular velocities:
+        omega_ix0 = omega_ix
+        omega_ix  = omega_ix0 + deltaOm_ix
+        omega_iy0 = omega_iy
+        omega_iy  = omega_iy0 + deltaOm_iy
+        omega_iz0 = omega_iz
+        omega_iz  = omega_iz0 + deltaOm_iz
         omega_jx0 = omega_jx
-        omega_jx = omega_jx0 + deltaOm_jx
+        omega_jx  = omega_jx0 + deltaOm_jx
         omega_jy0 = omega_jy
-        omega_jy = omega_jy0 + deltaOm_jy
+        omega_jy  = omega_jy0 + deltaOm_jy
         omega_jz0 = omega_jz
-        omega_jz = omega_jz0 + deltaOm_jz
-
-        v_i = v_ix*x_loc + v_iy*y_loc
-        v_j = v_jx*x_loc + v_jy*y_loc
+        omega_jz  = omega_jz0 + deltaOm_jz
         omega_i = omega_ix*x_loc + omega_iy*y_loc + omega_iz*z_loc
         omega_j = omega_jx*x_loc + omega_jy*y_loc + omega_jz*z_loc
-        # ball-ball friction:
-        u_iC = v_i - np.cross(r_ic, omega_i)
-        u_jC = v_j - np.cross(r_jc, omega_j)
+        # update ball-table slips:
+        u_iR = v_i + R * cross(_k, omega_i)
+        u_jR = v_j + R * cross(_k, omega_j)
+        u_iR_mag = sqrt(dot(u_iR, u_iR))
+        u_jR_mag = sqrt(dot(u_jR, u_jR))
+        u_iR_x = dot(u_iR, x_loc)
+        u_iR_y = dot(u_iR, y_loc)
+        u_jR_x = dot(u_jR, x_loc)
+        u_jR_y = dot(u_jR, y_loc)
+        # update ball-ball slip:
+        u_iC = v_i - cross(r_ic, omega_i)
+        u_jC = v_j - cross(r_jc, omega_j)
         u_ijC = u_jC - u_iC
-        u_ijC_x = np.dot(u_ijC, x_loc)
-        u_ijC_z = np.dot(u_ijC, z_loc)
-        u_ijC_xz_mag = np.sqrt(u_ijC_x**2 + u_ijC_z**2)
-        # ball-table friction:
-        u_iR = v_i + R * np.cross(_k, omega_i)
-        u_iR_mag = np.sqrt(np.dot(u_iR, u_iR))
-        u_iR_x = np.dot(u_iR, x_loc)
-        u_iR_y = np.dot(u_iR, y_loc)
-        u_jR = v_j + R * np.cross(_k, omega_j)
-        u_jR_mag = np.sqrt(np.dot(u_jR, u_jR))
-        u_jR_x = np.dot(u_jR, x_loc)
-        u_jR_y = np.dot(u_jR, y_loc)
+        u_ijC_xz = u_ijC - dot(u_ijC, y_loc) * y_loc
+        u_ijC_x = dot(u_ijC_xz, x_loc)
+        u_ijC_z = dot(u_ijC_xz, z_loc)
+        u_ijC_xz_mag = sqrt(dot(u_ijC_xz, u_ijC_xz))
+        v_ij = v_j - v_i
+        v_ijy = dot(v_ij, y_loc)
+        v_ix, v_iy = dot(v_i, x_loc), dot(v_i, y_loc)
+        v_jx, v_jy = dot(v_j, x_loc), dot(v_j, y_loc)
+        # increment work:
         deltaW = 0.5 * deltaP * deltaV_ijy
         W += deltaW
         niters += 1
-        _logger.debug('''
-        u_iR_mag = %s
-        u_jR_mag = %s
-        ''', u_iR_mag, u_jR_mag)
-
         if W_c is None and v_ijy > 0:
             W_c = W
             W_f = (1 + e**2) * W_c
--- test/test_collisions.py
+++ test/test_collisions.py
@@ -11,26 +11,21 @@ _k = np.array([0.0, 0.0, 1.0])
 
 def test_collide_balls():
     e = 0.9
-    mu_s = 0 #0.2
-    mu_b = 0 #0.06
+    mu_s = 0.2
+    mu_b = 0.06
     M = 0.17
     R = 1.125 * 0.0254
     r_i = np.array([0.0, 0.0, 0.0])
-    # rd = 1 - 2 * np.random.rand(3); rd[2] = 0
     rd = np.array([0.0, 1.0, 0.0])
-    rd_mag_sqrd = np.dot(rd, rd)
-    # while rd_mag_sqrd < 0.05:
-    #     rd = 1 - 2 * np.random.rand(3); rd[2] = 0
-    #     rd_mag_sqrd = np.dot(rd, rd)
-    r_j = r_i + 2 * R * rd / rd_mag_sqrd
+    r_j = r_i + 2 * R * rd
     r_ij = r_j - r_i
     r_ij_mag = np.sqrt(np.dot(r_ij, r_ij))
     y_loc = r_ij / r_ij_mag
     z_loc = _k
     x_loc = np.cross(y_loc, z_loc)
     r_c = r_i + R * y_loc
-    # v_i = 1 * rd - 0.1 * x_loc
-    v_i = 3 * rd
+    # v_i = 3 * rd
+    v_i = 0.5 * (0.5*np.sqrt(3)*rd + 0.5*x_loc)
     v_j = np.zeros(3, dtype=np.float64)
     omega_i = np.zeros(3, dtype=np.float64)
     omega_j = np.zeros(3, dtype=np.float64)
@@ -43,10 +38,19 @@ def test_collide_balls():
                                                    4000)
     v_ijy = np.dot(v_j, y_loc) - np.dot(v_i, y_loc)
     v_ijy1 = np.dot(v_j1, y_loc) - np.dot(v_i1, y_loc)
+    v_il = np.array([np.dot(v_i, x_loc), np.dot(v_i, y_loc), 0])
+    v_jl = np.array([np.dot(v_j, x_loc), np.dot(v_j, y_loc), 0])
+    v_i1l = np.array([np.dot(v_i1, x_loc), np.dot(v_i1, y_loc), 0])
+    v_j1l = np.array([np.dot(v_j1, x_loc), np.dot(v_j1, y_loc), 0])
     _logger.debug('''
 v_i  = %s
 v_i1 = %s
+v_j  = %s
+v_j1 = %s
 
+(local frame):
+v_i  = %s
+v_i1 = %s
 v_j  = %s
 v_j1 = %s
 
@@ -57,6 +61,7 @@ v_ijy1 = %s
 sqrt(|v_ijy1 / v_ijy|) = %s
 ''',
                   v_i, v_i1, v_j, v_j1,
+                  v_il, v_i1l, v_jl, v_j1l,
                   v_ijy, v_ijy1,
                   v_ijy1 / v_ijy,
                   np.sqrt(-v_ijy1 / v_ijy))
